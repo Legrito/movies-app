e4e7ca43dc924f3d3afbc897dd197f3b71cb9b34
Lesya <jamma@i.ua>
2023-01-12 15:52:01 +0100

    Fix classname for NavLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ export const Header = () => {
     return (
         <ul className="header__nav">
       <li>
-        <NavLink exact to="/" className="NavLink" activeClassName="NavLink--active" >Home</NavLink>
+        <NavLink exact to="/" className={({ isActive }) => `NavLink ${isActive ? "NavLink--active" : ""}`} >Home</NavLink>
       </li>
       <li>
-        <NavLink to="/movies" className="NavLink" activeClassName="NavLink--active" >Movies</NavLink>
+        <NavLink to="/movies" className={({ isActive }) => `NavLink ${isActive ? "NavLink--active" : ""}`}>Movies</NavLink>
       </li>
     </ul>
     );
