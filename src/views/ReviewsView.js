d7f3b7e38594f81636c11d249ea0037a37130522
Lesya <jamma@i.ua>
2023-02-13 15:26:37 +0100

    Replace ReviewsView class with func component

--- src/views/ReviewsView.js
+++ src/views/ReviewsView.js
@@ -1,50 +1,50 @@
-import { Component } from 'react';
+import { Component, useEffect } from 'react';
+import { useState } from 'react';
 import { MovieLoader } from '../components/Loader';
 import { getMovieAdditionalInfo } from '../services/ApiServices';
 
-class ReviewsView extends Component {
-  state = {
-    reviews: [],
-    loader: false
-  };
+const ReviewsView = ({ movieId }) => {
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async componentDidMount() {
-    try {
-      this.setState({ loader: true });
-      const response = await getMovieAdditionalInfo(this.props.movieId, 'reviews');
-      this.setState({ reviews: response.data.results });
-      window.scrollBy({
-        top: 400,
-        behavior: 'smooth'
-      });
-    } catch (err) {
-      console.log(err.message);
-    } finally {
-      this.setState({ loader: false });
-    }
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await getMovieAdditionalInfo(movieId, 'reviews');
+        setReviews(response.data.results);
+        window.scrollBy({
+          top: 400,
+          behavior: 'smooth'
+        });
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  render() {
-    const { reviews, loader } = this.state;
-    return (
-      <>
-        <h2>Reviews</h2>
-        {loader && <MovieLoader />}
-        {reviews.length !== 0 ? (
-          <ul>
-            {reviews.map((review) => (
-              <li key={review.id}>
-                <h3>{review.author}</h3>
-                <p>{review.content}</p>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          'No reviews'
-        )}
-      </>
-    );
-  }
-}
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>Reviews</h2>
+      {isLoading && <MovieLoader />}
+      {reviews.length !== 0 ? (
+        <ul>
+          {reviews.map((review) => (
+            <li key={review.id}>
+              <h3>{review.author}</h3>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        'No reviews'
+      )}
+    </>
+  );
+};
 
 export default ReviewsView;
