7edcec5a78b4fc1a76664e99fa75c666f7631777
Lesya <jamma@i.ua>
2023-02-13 14:07:42 +0100

    Refactored HomeView component from class to functional

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,40 +1,40 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { LoadMore } from '../components/LoadMore/LoadMore';
 import MovieCard from '../components/MovieCard/MovieCard';
 import { MovieLoader } from '../components/Loader';
 import { getMovieTrends } from '../services/ApiServices';
 
-class HomeView extends Component {
-  state = {
-    movies: [],
-    loader: false,
-    page: 1
-  };
+const HomeView = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
-  async componentDidMount() {
-    try {
-      this.setState({ loader: true });
-      const response = await getMovieTrends('day', this.state.page);
-      this.setState({ movies: response.data.results });
-    } catch (err) {
-      console.log(err.messsage);
-    } finally {
-      this.setState({ loader: false });
-    }
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await getMovieTrends('day', page);
+        setMovies(response.data.results);
+      } catch (err) {
+        console.log(err.messsage);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  onLoadMore = () => {
-    this.setState({ loader: true });
-    getMovieTrends('day', this.state.page + 1)
+    fetchData();
+  }, []);
+
+  const onLoadMore = () => {
+    setIsLoading(true);
+    getMovieTrends('day', page + 1)
       .then((response) => {
-        this.setState((prevState) => ({
-          movies: [...prevState.movies, ...response.data.results],
-          page: prevState.page + 1
-        }));
+        setMovies(prevState => [...prevState, ...response.data.results]);
+        setPage(prevState => prevState + 1);
       })
       .catch((err) => console.log(err))
       .finally(() => {
-        this.setState({ loader: false });
+        setIsLoading(false);
         window.scrollTo({
           top: document.querySelector('.trend-list').scrollHeight,
           behavior: 'smooth'
@@ -42,17 +42,15 @@ class HomeView extends Component {
       });
   };
 
-  render() {
-    return (
-      <section className="trend__section">
-        <h1 className="trend__title">Trending today</h1>
-        {/* { this.state.loader ? <MovieLoader /> 
+  return (
+    <section className="trend__section">
+      <h1 className="trend__title">Trending today</h1>
+      {/* { this.state.loader ? <MovieLoader />
                 : <MoviesList movies={this.state.movies} /> } */}
-        {this.state.loader ? <MovieLoader /> : <MovieCard movies={this.state.movies} />}
-        {this.state.movies.length > 0 && <LoadMore onClick={this.onLoadMore} />}
-      </section>
-    );
-  }
-}
+      {isLoading ? <MovieLoader /> : <MovieCard movies={movies} />}
+      {movies.length > 0 && <LoadMore onClick={onLoadMore} />}
+    </section>
+  );
+};
 
 export default HomeView;
